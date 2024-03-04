d13648a82d52d1027416842affb544587499a827
Ruslan Tsuman <r.ts@me.com>
2024-03-04 11:08:31 +0100

    2

--- dist/concatenation.js
+++ dist/concatenation.js
@@ -1,11 +1,5 @@
 function concatenation(firstWord, secondWord) {
-    console.log('====================================');
-    console.log('====================================');
-    console.log('====================================');
-    console.log('====================================');
-    console.log(`${firstWord} ${secondWord}`);
-    console.log('====================================');
+	console.log(`${firstWord} ${secondWord}`);
 }
-;
 export { concatenation };
 //# sourceMappingURL=concatenation.js.map
